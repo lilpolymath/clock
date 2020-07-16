448192455aeb7b173551c7e455395c4c1b81d4d3
Adedapo Ayobami <ayobami.adedapo@mkobo.com.ng>
2020-07-16 21:22:39 +0100

    feat: basic clock setup

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setInterval(function() {
   var date = new Date(),
-    clock = document.querySelector('.control');
+    control = document.querySelector('.control');
 
   const hours = date.getHours();
   const minutes = date.getMinutes();
@@ -11,6 +11,37 @@ setInterval(function() {
   const angles = [minuteAngle, hourAngle, minuteAngle];
 
   angles.forEach(function(rotation, index) {
-    clock.children[index].style.transform = 'rotate(' + rotation + 'deg)';
+    control.children[index].style.transform = 'rotate(' + rotation + 'deg)';
   });
 }, 1000);
+
+function positionNumbers() {
+  let clock = document.querySelector('.clock');
+
+  console.log(clock.children);
+
+  let number = 12; //
+  let size = 200; //
+
+  let half = size / 2;
+
+  let cx = half; // center of x
+  let cy = half; // center of y
+  let r = half; // radius
+
+  for (let i = 1; i <= number; i++) {
+    const ang = i * (Math.PI / (number / 2));
+    const leftPos = cx + r * Math.cos(ang);
+    const topPos = cy + r * Math.sin(ang);
+    // clock.children[i].style.transform = 'translate(-150%, -100%)';
+
+    // if (clock.children[i].textContent.length === 2) {
+    //   clock.children[i].style.transform = 'translate(-125%, -100%)';
+    // }
+    // clock.children[i].style.top = topPos + 'px';
+    // clock.children[i].style.left = leftPos + 'px';
+    clock.children[i].style.zIndex = 10;
+  }
+}
+
+// positionNumbers();
